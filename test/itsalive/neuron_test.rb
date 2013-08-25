ff4e331bd6737a29d134d8c071571eedbcd4d0a5
Vasiliy Yorkin <vyorkin@undev.ru>
2013-08-26 00:49:30 +0400

    change test name

--- test/itsalive/neuron_test.rb
+++ test/itsalive/neuron_test.rb
@@ -1,12 +1,12 @@
 require_relative '../minitest_helper'
 
 class NeuronTest < Minitest::Test
-  def setup 
+  def setup
     @subject = ItsAlive::Neuron.new([0.1, 0.2, 0.3], 1)
     @signals = [1, 2, 3]
   end
 
-  def test_that_it_activates_using_default_sigmoid_function
+  def test_that_it_correctly_activates_using_sigmoid_function
     assert { @subject.activate(@signals) == 0.9168273035060777 }
   end
 end
