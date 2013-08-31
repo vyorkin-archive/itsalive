c6c88afbce14273ed60f70591b8a127ee9914a1b
Vasiliy Yorkin <vyorkin@undev.ru>
2013-08-31 23:38:42 +0400

    add input & output synapses

--- lib/itsalive/synapse.rb
+++ lib/itsalive/synapse.rb
@@ -1,5 +1,12 @@
 module ItsAlive
   class Synapse
+    class << self
+      def input_to(target) Synapse.new(nil, target) end
+      def output_from(source) Synapse.new(source, nil) end
+
+      alias_method :receptor_to, :input_to
+    end
+
     attr_reader :source, :target
     attr_reader :weight, :output
 
